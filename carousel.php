94bd825d1a5dbb934e95ca691e6b7310377733fb
David <mbadavid212@gmail.com>
2025-09-22 12:44:27 +0100

    Carousel.php committed!

--- carousel.php
+++ carousel.php
@@ -1,29 +1,30 @@
-<?php
-/*
-Plugin Name: Carousel
-Description : A plugin to display a configurable image carousel via shortcode.
-Version: 1.0
-Author: David Mba
+<?php 
+/*Plugin Name: carousel
+ *Description: Adds a responsive image carousel to your site using a simple shortcode. Includes customizable settings for autoplay, speed and navigation.
+ *Version: 1.0
+ *Author: David Mba
 */
 
-// define constants for path
-define('CAROUSEL_PLUGIN_PATH', plugin_dir_path(__FILE__));
-define('CAROUSEL_PLUGIN_URL', plugin_dir_url(__FILE__));
+//Define constants for path
+define('PLUGIN_PATH', plugin_dir_path(__FILE__));
+define('PLUGIN_URL', plugin_dir_url(__FILE__));
 
-// include plugin components
-require_once CAROUSEL_PLUGIN_PATH . 'admin/settings-page.php';
-require_once CAROUSEL_PLUGIN_PATH . 'includes/shortcode-handler.php';
+//Add plugin components
+require_once PLUGIN_PATH . 'admin/settings-page.php';
+require_once PLUGIN_URL . 'includes/shortcode-handler.php';
 
-// enqueue styles and scripts
-function carousel_enqueue_scripts() {
-	wp_enqueue_style('carousel-style', CAROUSEL_PLUGIN_URL. 'assets/css/carousel-style.css');
-	wp_enqueue_script('carousel-script', CAROUSEL_PLUGIN_URL. 'assets/js/carousel-script.js', array('jquery'), null, true);
+//enqueue style and script
+function carousel_enqueue_assets() {
+	wp_enqueue_style('carousel-style', PLUGIN_URL . 'assets/css/carousel-style.css');
+	wp_enqueue_script('carousel-script', PLUGIN_URL . 'assets/js/carousel-script.js', array('jquery'), null, true);
 }
+
+// connect enqueueing function to wordpress enqueueing scripts hook
 add_action('wp_enqueue_scripts', 'carousel_enqueue_assets');
 
-// Optional: Add activation hook
-function carousel_activate() {
-	// you can initialize default settings here
-
+//Activation hook
+function carousel_activate(){
+	//for default settings
 }
+
 register_activation_hook(__FILE__, 'carousel_activate');
